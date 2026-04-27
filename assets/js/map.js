6cbbefd1c032337a4d76e7f7f6be7b369fd102f0
Bojamma K R <prathibojamma@gmail.com>
2026-04-27 21:54:49 +0530

    Refactor map initialization and update functions

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,24 +1,17 @@
 let map;
 let marker;
 
-function initMap(lat, lng) {
-  if (map) return;
-
-  map = L.map('map').setView([lat, lng], 15);
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap'
-  }).addTo(map);
-
-  marker = L.marker([lat, lng]).addTo(map);
-}
-
-function updateMap(lat, lng) {
+window.updateMap = function(lat, lng) {
   if (!map) {
-    initMap(lat, lng);
-    return;
-  }
+    map = L.map('map').setView([lat, lng], 15);
 
-  marker.setLatLng([lat, lng]);
-  map.setView([lat, lng], 15);
-}
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap'
+    }).addTo(map);
+
+    marker = L.marker([lat, lng]).addTo(map);
+  } else {
+    marker.setLatLng([lat, lng]);
+    map.setView([lat, lng], 15);
+  }
+};
